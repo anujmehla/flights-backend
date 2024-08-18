ef1d8645892e271285632eef2004eb05c2fb7b5a
Anuj Mehla <anujmehla@gmail.com>
2024-08-18 16:36:26 +0530

    initial commit

---
+++ office-manager-service/office-manager-service/src/main/java/com/flight/office_manager_service/OfficeManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flight.office_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeManagerServiceApplication.class, args);
+	}
+
+}
---
+++ office-manager-service/office-manager-service/src/test/java/com/flight/office_manager_service/OfficeManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight.office_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
